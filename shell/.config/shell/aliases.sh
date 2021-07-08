3b49633dc23870c4cb4a8a7fb2a457c792d15b5d
Azarattum <azarattum@pm.me>
2021-07-08 10:38:57 +0700

    Updated btops & aliases

--- shell/.config/shell/aliases.sh
+++ shell/.config/shell/aliases.sh
@@ -1,7 +1,7 @@
 alias nv=nvim
 alias wget="wget --hsts-file=$XDG_CACHE_HOME/wget-hsts"
 alias fuck="sudo \$(history | tail -n 1 | cut -c 8-)"
-alias cs="ssh root@185.211.247.52"
+alias cs="ssh root@194.169.160.199"
 alias py=python
 alias yt="ytfzf -t"
 alias lf=lf-ueberzug
